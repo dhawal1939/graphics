57d9515a86cae17c07de0d7bd3285e1b8e6270f1
thesigmaguy <sdhawal2k8@gmail.com>
2020-06-11 14:43:05 +0530

    trianglemoderngl

--- graphics-moderngl/graphics-moderngl/sample_triangle.py
+++ graphics-moderngl/graphics-moderngl/sample_triangle.py
@@ -1,12 +1,53 @@
 import moderngl
 import moderngl_window
+import numpy as np
 
-class window(moderngl_window.WindowConfig):
-    def __init__(self, ctx=None, wnd=None, timer=None, **kwargs):
-        self.gl_version = kwargs['version']
-        self.window_size =
-        super().__init__(ctx=ctx, wnd=wnd, timer=timer, **kwargs)
+class window( moderngl_window.WindowConfig ):
+    gl_version = (4, 3)
+    resizable = True
+    aspect_ratio = 16/9
+    window_size = (1920, 1080)
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
 
     @classmethod
     def run(cls):
         moderngl_window.run_window_config(cls)
+
+
+class hello(window):
+    gl_version = (4, 3)
+    title='hello'
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+        self.prog = self.ctx.program( vertex_shader='''
+                #version 330
+                in vec2 in_vert;
+                void main() {
+                    gl_Position = vec4(in_vert, 0.0, 1.0);
+                }
+            ''',
+            fragment_shader='''
+                #version 330
+                out vec4 f_color;
+                void main() {
+                    f_color = vec4(0.3, 0.5, 1.0, 1.0);
+                }
+            ''',)
+        vertices = np.array([
+            0.0, 0.8,
+            -0.6, -0.8,
+            0.6, -0.8,
+        ])
+
+        self.vbo = self.ctx.buffer(vertices.astype('f4').tobytes())
+        self.vao = self.ctx.simple_vertex_array(self.prog, self.vbo, 'in_vert')
+
+    def render(self, time: float, frame_time: float):
+        self.ctx.clear(1.0,1.0,1.0)
+        self.vao.render()
+
+hello.run()
