30e1a8b623f68a69a19f9f577241eb6cdadbe7fb
thesigmaguy <sdhawal2k8@gmail.com>
2020-06-21 12:17:49 +0530

    transforms-finish

--- graphics-moderngl/1.5-Transforms/transforms.py
+++ graphics-moderngl/1.5-Transforms/transforms.py
@@ -1,7 +1,8 @@
 import numpy as np
 from pathlib import Path
 import moderngl_window
-from pyrr import Matrix44 as mat4
+import glm
+
 
 class transforms(moderngl_window.WindowConfig):
     gl_version = (4, 3)
@@ -64,7 +65,6 @@ class transforms(moderngl_window.WindowConfig):
             (self.tex_vbo, '2f', 'in_tex_corr')
         ]
 
-
         self.vao = self.ctx.vertex_array(self.program, vao_content, self.ibo)
 
     def render(self, time: float, frame_time: float):
@@ -72,11 +72,17 @@ class transforms(moderngl_window.WindowConfig):
 
         import math
         self.program['uni_color'] = tuple([math.sin(time) / 2.0 + .5] * 3 + [1.0])
-        translate = mat4.from_translation([.5, 0.5, .0], dtype='float32')
-        rotation = mat4.from_eulers((time, time/2, time/3), dtype='float32')
+        model_view = glm.mat4(1.0)
+        model_view = glm.translate(model_view, glm.vec3(.5, .5, .0))
+        model_view = glm.rotate(model_view, glm.radians(90), glm.vec3(.0, .0, -1.0))
 
-        self.program['transform'].write(translate * rotation)
+        #if it needs to be continually rotated replae glm.radians with the time.
+        #model_view = glm.rotate(model_view, time, glm.vec3(.0, .0, -1.0))
 
+        scale_amount = math.sin(time)
+        model_view = glm.scale(model_view, glm.vec3([scale_amount]*3))
+
+        self.program['transform'].write(model_view)
 
         self.texture_fg.use(1)
         self.texture_bg.use(0)
